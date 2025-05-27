26e77b9ff133babbe557c3ba1f4a3462cd19952c
AfriCodeWizard <73635175+AfriCodeWizard@users.noreply.github.com>
2025-05-27 06:31:23 -0700

    Implement playGame function: - Moved score tracking and playGround functioninside playGame - Looped through 5 rounds with fresh player/computer choices - Declared final winner based on scores

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0; 
-
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
@@ -14,26 +11,46 @@ function getHumanChoice() {
 }
 //console.log(getHumanChoice());
 
-function playRound(humanChoice, computerChoice) { 
-    humanChoice = humanChoice.toLowerCase();
 
-    if (humanChoice === computerChoice) {
-        console.log("We have a tie!");
-    } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "scissors" && computerChoice === "paper") ||
-        (humanChoice === "paper" && computerChoice === "rock")
-    ) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore += 1;
-    } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore += 1; 
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0; 
+
+    function playRound(humanChoice, computerChoice) { 
+        humanChoice = humanChoice.toLowerCase();
+    
+        if (humanChoice === computerChoice) {
+            console.log("We have a tie!");
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "scissors" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "rock")
+        ) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore += 1;
+        } else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore += 1; 
+        }
+        console.log(`Score → Human: ${humanScore} | Computer: ${computerScore}`);
+    
     }
-    console.log(`Score → Human: ${humanScore} | Computer: ${computerScore}`);
 
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice(); 
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+
+        if (humanScore > computerScore) {
+            console.log("You win the game!"); 
+        } else if (computerScore > humanScore) {
+            console.log("You lose the game!"); 
+        } else {
+            console.log("We have a tie!"); 
+        }
+    }
 }
-const humanSelection = getHumanChoice(); 
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+playGame(); 
